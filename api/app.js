3260ddfc4a78c4ba2f9134731f0f7f70d1c5ece4
007shohanur <shohanur007rm@gmail.com>
2021-09-02 20:10:59 +0600

    error add

--- api/app.js
+++ api/app.js
@@ -24,24 +24,33 @@ const displaysearchResult = books => {
    // input search fild result show:
     const searchResult = document.getElementById('search-result');
     // clear dom:
+    error.innerHTML = '';
     searchResult.innerHTML = '';
-    // call forEach
-   books.forEach(book => {
-       console.log(book);
-       const div = document.createElement('div');
-       div.classList.add('col');
-       div.innerHTML = `
-    <div class="card h-100 p-3 border rounded-3">
-       <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top" alt="...">
-       <div class="card-body">
-         <h5 class="card-title">Book Title: ${book.title}</h5>
-         <h5 class="card-title">Author Name: ${book.author_name}</h5>
-         <h5 class="card-title">First Publish Year: ${book.first_publish_year}</h5>
-         <h5 class="card-title">Publisher: ${book.publisher}</h5>
-       </div>
-     </div>`;
-    //  call append child:
-     searchResult.appendChild(div);
-   })
-    
-}
+    // error message
+    if(books.length === 0){
+      const error = document.getElementById('error');
+      const div =document.createElement('div');
+      div.innerHTML = `<p>No Results Found</p>`;
+      error.append(div);
+    }
+    else{
+      books.forEach(book => {
+        console.log(book);
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `
+     <div class="card h-100 p-3 border rounded-3">
+        <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Book Title: ${book.title}</h5>
+          <h5 class="card-title">Author Name: ${book.author_name}</h5>
+          <h5 class="card-title">First Publish Year: ${book.first_publish_year}</h5>
+          <h5 class="card-title">Publisher: ${book.publisher}</h5>
+        </div>
+      </div>`;
+     //  call append child:
+      searchResult.appendChild(div);
+      
+    })
+    }
+  }
